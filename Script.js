86c2add07d0bed73332bb6361a52a04758bdd8b8
mariococ1994-design <mariococ1994@gmail.com>
2026-06-07 11:13:46 -0600

    Add function to display AI definition

---
+++ Script.js
@@ -0,0 +1,10 @@
+function mostrarRespuesta(){
+
+    let respuesta = document.getElementById("respuesta");
+
+    respuesta.innerHTML =
+    " IA significa Inteligencia Artificial.";
+
+    respuesta.style.color = "black";
+    respuesta.style.fontWeight = "bold";
+    respuesta.style.fontSize = "15px";
